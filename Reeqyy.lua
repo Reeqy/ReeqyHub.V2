6b715312974686f8c62d6d49c97e92a942fcf598
Reeqy <109089460+Reeqy@users.noreply.github.com>
2022-07-27 13:25:49 +0100

    Create Reeqyy.lua

---
+++ Reeqyy.lua
@@ -0,0 +1,90 @@
+
+
+
+
+
+-- CREDITS TO ALL SCRIPT OWNERS!
+
+
+
+
+
+
+
+
+
+local Flux = loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/fluxlib.txt")()
+
+
+
+local win = Flux:Window("ReeqyHub", "UwU", Color3.fromRGB(255, 110, 48), Enum.KeyCode.LeftControl)
+local tab = win:Tab("Dahood", "http://www.roblox.com/asset/?id=6023426915")
+
+tab:Label("Dahood scripts!.")
+tab:Line()
+
+
+tab:Button("Swagmode", "This function may not work sometimes and you can get Kicked.", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/lerkermer/lua-projects/master/BETA_Swagmode'))()
+
+Flux:Notification("Loading..", "Alright")
+end)
+
+tab:Button("Dimag X", "This function may not work sometimes and you can get Kicked.", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Dimag16/DimagX_NEW/main/dimagx', true))()
+
+    Flux:Notification("Loading..", "Alright")
+end)
+
+    tab:Button("Plasma", "This function may not work sometimes and you can get Kicked.", function()
+        loadstring(game:HttpGet("https://iexploit.xyz/scripts/plasma/loader"))()
+
+        Flux:Notification("Loading..", "Alright")
+    end)
+
+
+
+    tab:Button("Artic v9.7", "This function may not work sometimes and you can get Kicked.", function()
+        loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/PolarWasHere/Arctic/main/Arctic"))()
+
+
+
+        Flux:Notification("Loading..", "Alright")
+    end)
+
+
+
+    tab:Button("AimLock", "This function may not work sometimes and you can get Kicked.", function()
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/HuskyLimited/Script/main/QitkotLock.lua', true))()
+
+
+
+        Flux:Notification("Loading..", "Alright")
+    end)
+
+
+
+
+
+
+    tab:Button("Faded", "This function may not work sometimes and you can get Kicked.", function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/NighterEpic/Faded/main/YesEpic", true))()
+
+
+
+        Flux:Notification("Loading..", "Alright")
+    end)
+
+
+    tab:Button("Destiny", "This function may not work sometimes and you can get Kicked.", function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/compwnter/destiny/main/loader"))()
+
+
+
+        Flux:Notification("Loading..", "Alright")
+    end)
+
+
+
+
+
