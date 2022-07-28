a5f608ab1c263e5de28e3eddb72dcf1f38d2f44d
Reeqy <109089460+Reeqy@users.noreply.github.com>
2022-07-28 15:52:02 +0100

    Update Reeqyy.lua

--- Reeqyy.lua
+++ Reeqyy.lua
@@ -12,16 +12,56 @@
 
 
 
-
 local Flux = loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/fluxlib.txt")()
 
 
 
 local win = Flux:Window("ReeqyHub", "UwU", Color3.fromRGB(255, 110, 48), Enum.KeyCode.LeftControl)
+local Maintab = win:Tab("Universal", "http://www.roblox.com/asset/?id=6023426915")
+
+
 local tab = win:Tab("Dahood", "http://www.roblox.com/asset/?id=6023426915")
 
-tab:Label("Dahood scripts!.")
-tab:Line()
+
+Maintab:Button("Infinite Yield", "This function may not work sometimes and you can get Kicked.", function()
+    loadstring(game:HttpGet(('https://pastebin.com/raw/K9uLn50M'),true))()
+
+    Flux:Notification("Loading..", "Alright")
+end)
+
+
+
+Maintab:Button("FE GUI ", "This function may not work sometimes and you can get Kicked.", function()
+    loadstring(game:HttpGet("https://pastebin.com/raw/VZJEYiJF", true))()
+    
+    Flux:Notification("Loading..", "Alright")
+end)
+
+
+
+
+
+
+
+Maintab:Button("Trolling GUI", "This function may not work sometimes and you can get Kicked.", function()
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/Henry887/RemX-Script-Hub/main/main.lua'),true))()
+
+    Flux:Notification("Loading..", "Alright")
+end)
+
+
+
+
+
+tab:Button("Swagmode", "This function may not work sometimes and you can get Kicked.", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/lerkermer/lua-projects/master/BETA_Swagmode'))()
+
+Flux:Notification("Loading..", "Alright")
+end)
+
+
+
+
 
 
 tab:Button("Swagmode", "This function may not work sometimes and you can get Kicked.", function()
