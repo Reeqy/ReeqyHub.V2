48a5acc269b7448ba8665a4768251c132b39bb3a
Reeqy <109089460+Reeqy@users.noreply.github.com>
2022-08-08 14:37:54 +0100

    Create a.lua

---
+++ a.lua
@@ -0,0 +1,33 @@
+]=_[f];e=e+o;_=d[e];l[_[b]]=_[f];e=e+o;_=d[e];l[_[x]]=_
+[y];e=e+o;_=d[e];n=_[b]l[n]=l[n](a(l,n+o,_[y]))e=e+o;_=d[e];l[_[b]][_[u]]=l[
+    _[I]];e=e+o;_=d[e];l[_[c]]=h[_[s]];e=e+o;_=d[e];l[_[r]]=_[t];e=e+o;_=d[e];l[_[c]]=_[y];e=e+o;_=d[e];l[_[c]]=_[y];e=e+o;_
+    =d[e];n=_[b]l[n]=l[n](a(l,n+o,_[t]))e=e+o;_=d[e];l[_[r]][_[t]]=l[_[O]];e=e+o;_=d[e];l[_[i]][_[s]]=_[I];e=e+o;_=d[e];n=_[
+        k]l[n](a(l,n+g,_[s]))e=e+o;_=d[e];do return end;break end while 924==(n)/((97887/0x43))do local c;local k,w;local t;
+            local n;l[_[b]]=h[_[u]];e=e+o;_=d[e];l[_[i]]=h[_[y]];e=e+o;_=d[e];n=_[i];t=l[_[m]];l[n+1]=t;l[n]=t[_[I]];e=e+o;_
+            =d[e];l[_[x]]=h[_[f]];e=e+o;_=d[e];l[_[i]]=_[s];e=e+o;_=d[e];l[_[r]]=_[f];e=e+o;_=d[e];l[_[r]]=_[u];e=e+o;_=d[
+                e];n=_[r]l[n]=l[n](a(l,n+o,_[f]))e=e+o;_=d[e];l[_[x]]=(_[m]~=0);e=e+o;_=d[e];n=_[r]k,w=M(l[n](a(l,n+1,
+                _
+                [y])))p=w+n-1 c=0;for _=n,p do c=c+o;l[_]=k[c];end;e=e+o;_=d[e];n=_[i]l[n]=l[n](a(l,n+o,p))e=e+o;_=d[
+                    e];l[_[r]]();e=e+o;_=d[e];do return end;break end;break;end break;end break;end break;end break
+                    ;end break;end while 1943==(n)/((72498/0x2b))do n=(5142984)while w<=(-0x3d+147)do n=-n n=(16156556)while w<
+                        =((0x10f8-2214)/30)do n=-n n=(725254)while(2688/0x2a)>=w do n=-n n=(3834966)while(216-0x9c)>=w do n=-
+                            n n=(5139700)while w<=(0x27e0/176)do n=-n local t;local v,g;local w;local n;l[_[x]]=h[_[s]];e=
+                                e+o;_=d[e];l[_[k]]=h[_[u]];e=e+o;_=d[e];n=_[r];w=l[_[f]];l[n+1]=w;l[n]=w[_[B]];e=e+o;_=
+                                d[e];l[_[k]]=h[_[s]];e=e+o;_=d[e];l[_[x]]=_[m];e=e+o;_=d[e];l[_[b]]=_[f];e=e+o;_=d[e
+                                ];l[_[x]]=_[m];e=e+o;_=d[e];n=_[k]l[n]=l[n](a(l,n+o,_[u]))e=e+o;_=d[e];l[_[i]]=(
+                                    _[y]~=0);e=e+o;_=d[e];n=_[i]v,g=M(l[n](a(l,n+1,_[s])))p=g+n-1 t=0;for _=n,
+                                    p do t=t+o;l[_]=v[t];end;e=e+o;_=d[e];n=_[c]l[n]=l[n](a(l,n+o,p))e=e
+                                    +o;_=d[e];l[_[x]]();e=e+o;_=d[e];do return end;break;end while(n)
+                                    /((-102+0x832))==2575 do n=(2276105)while((216-0x7e)+-#"Boostd
+                                        d moonse
+                                        c for 
+                                        meme s
+                                        tring"
+                                    )<w do n=-n
+                                         local 
+
+                                         s;local w,
+                                         y;local v;local n;l[_[c]]=h[_[m]];e=e+o;_=d[e];l[_[i
+                                         ]]=h[_[t]];e=e+o;_=d[e];n=_[c];v=l[_[t]];l[n+1]=v;l[n]=v[_
+                                         [z]];e=e+o;_=d[e];l[_[b]]=h[_[f]];e=e+o;_=d[e]
+                        
